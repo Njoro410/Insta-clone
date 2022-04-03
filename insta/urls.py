9937d309049e5c760c306b98ac002200852cd59d
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-03 14:07:03 +0300

    added register url

--- insta/urls.py
+++ insta/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index, name='home'),
+    path('user_registration',views.register, name ='user_registration' )
 ]
 
 if settings.DEBUG:
