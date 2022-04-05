0b95bf648c09128e075cc0d97afcf5033ced015d
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-05 19:51:10 +0300

    added  an admin file

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Post,Comments,UserFollowing
 
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(UserFollowing)
