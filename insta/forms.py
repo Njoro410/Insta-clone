384a460e0e2245662cf9a6c1c05b8a9c17816f0c
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-05 19:51:37 +0300

    added a forms file

--- insta/forms.py
+++ insta/forms.py
@@ -1,6 +1,10 @@
+from dataclasses import fields
 from django import forms
+from django.forms import ModelForm
+from .models import Comments, Post
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -9,4 +13,18 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+        
+        
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('profile',)
+        fields = ('image','name','caption')
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        
+        fields = ('content',)
         
