09706d6216bb90e32da7f19fab76b478e3f11a19
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-05 19:52:47 +0300

    added urls for app

--- insta/urls.py
+++ insta/urls.py
@@ -1,11 +1,26 @@
+import profile
 from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView, PostDeleteView, ProfileView, AddFollower,RemoveFollower,CommentCreateView
 
 urlpatterns = [
+    
+    # path('',PostListView.as_view(), name='home'),
     path('',views.index, name='home'),
-    path('user_registration',views.register, name ='user_registration' )
+    path('profile/<int:pk>',ProfileView.as_view(),name = 'profile'),
+    path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/',PostCreateView.as_view(), name='post-create'),
+    path('user_registration/',views.register, name ='user_registration' ),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name = 'accounts/login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'accounts/logout.html'), name='logout'),
+    path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
+    path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+    path('post/<int:pk>/comment/',CommentCreateView.as_view(), name='add_comment'),
 ]
 
 if settings.DEBUG:
